f27dab45d844bd46e59f2ede90b3c6c1f2bc3099
Ioseb Koplatadze <soso.kop@gmail.com>
2021-10-28 13:48:44 +0400

    cache & sync data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { isEqual } from 'lodash-es';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pwa';
+export class AppComponent implements OnInit {
+  keys = Object.keys;
+  todos: any;
+  loading = true;
+  needSync = false;
+
+  constructor(private client: HttpClient) {}
+
+  ngOnInit(): void {
+    this.client
+      .get<any>('http://localhost:8080/todos')
+      .pipe(
+        finalize(() => {
+          console.log('todos = ', this.todos);
+          this.loading = false;
+        })
+      )
+      .subscribe(
+        (todos) => {
+          console.log('get data from api');
+          const cachedData = this.cachedData;
+          this.needSync = cachedData && !isEqual(cachedData, todos);
+          this.todos = this.needSync ? cachedData : todos;
+        },
+        () => {
+          this.todos = this.cachedData;
+          console.log('get data from storage');
+        }
+      );
+  }
+
+  onSave(): void {
+    this.loading = true;
+    this.client
+      .post<any>('http://localhost:8080/todos', this.todos)
+      .pipe(
+        finalize(() => {
+          console.log('todos = ', this.todos);
+          this.cacheData();
+          this.loading = false;
+        })
+      )
+      .subscribe(() => {
+        this.needSync = false;
+      });
+  }
+
+  private get cachedData(): any {
+    return JSON.parse(localStorage.getItem('todos') ?? 'null');
+  }
+
+  private cacheData(): void {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
